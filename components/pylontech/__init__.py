7636e0663058e25599b90fad38842b504369dc99
だから <82636574+Dackara@users.noreply.github.com>
2024-01-05 11:39:34 +0100

    Update __init__.py

--- components/pylontech/__init__.py
+++ components/pylontech/__init__.py
@@ -1,34 +1,46 @@
 import logging
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import uart
+import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
 _LOGGER = logging.getLogger(__name__)
 
+CODEOWNERS = ["@functionpointer"]
 DEPENDENCIES = ["uart"]
-CODEOWNERS = ["@Dackara"]
-AUTO_LOAD = ["binary_sensor", "text_sensor", "sensor"]
 MULTI_CONF = True
 
 CONF_PYLONTECH_ID = "pylontech_id"
+CONF_BATTERY = "battery"
 
-pylontech_ns = cg.esphome_ns.namespace("pylontech")*
+pylontech_ns = cg.esphome_ns.namespace("pylontech")
+PylontechComponent = pylontech_ns.class_(
+    "PylontechComponent", cg.PollingComponent, uart.UARTDevice
+)
+PylontechBattery = pylontech_ns.class_("PylontechBattery")
+
+CV_NUM_BATTERIES = cv.int_range(1, 6)
 
 PYLONTECH_COMPONENT_SCHEMA = cv.Schema(
     {
-        cv.Required(CONF_PYLONTECH_ID): cv.use_id(PylontechComponent),
+        cv.GenerateID(CONF_PYLONTECH_ID): cv.use_id(PylontechComponent),
+        cv.Required(CONF_BATTERY): CV_NUM_BATTERIES,
     }
 )
 
+
 CONFIG_SCHEMA = cv.All(
-    cv.Schema({cv.GenerateID(): cv.declare_id(PylontechComponent),})
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(PylontechComponent),
+        }
+    )
     .extend(cv.polling_component_schema("60s"))
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)

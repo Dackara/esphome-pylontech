81a0a547ba6a57040c9780d7c3529f551f82e489
だから <82636574+Dackara@users.noreply.github.com>
2024-01-06 21:48:38 +0100

    Update __init__.py

--- components/pylontech/text_sensor/__init__.py
+++ components/pylontech/text_sensor/__init__.py
@@ -16,6 +16,10 @@ CONF_BASE_STATE = "base_state"
 CONF_VOLTAGE_STATE = "voltage_state"
 CONF_CURRENT_STATE = "current_state"
 CONF_TEMPERATURE_STATE = "temperature_state"
+CONF_DATE_STATE = "date_state"
+CONF_TIME_STATE = "time_state"
+CONF_BV_STATE = "bv_state"
+CONF_BT_STATE = "bt_state"
 CONF_MOS_STATE = "mos_state"
 
 MARKERS: list[str] = [
@@ -23,6 +27,10 @@ MARKERS: list[str] = [
     CONF_VOLTAGE_STATE,
     CONF_CURRENT_STATE,
     CONF_TEMPERATURE_STATE,
+    CONF_DATE_STATE,
+    CONF_TIME_STATE,
+    CONF_BV_STATE,
+    CONF_BT_STATE,
     CONF_MOS_STATE,
 ]
 

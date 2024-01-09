5cac5603c25cacb8dd8a782c0be132c2ed924bad
だから <82636574+Dackara@users.noreply.github.com>
2024-01-09 06:20:53 +0100

    Update pylontech.h

--- components/pylontech/pylontech.h
+++ components/pylontech/pylontech.h
@@ -13,11 +13,54 @@ static const uint8_t TEXT_SENSOR_MAX_LEN = 8;
 class PylontechListener {
  public:
   struct LineContents {
-    int bat_num = 0, volt, curr, tempr, tlow, thigh, vlow, vhigh, coulomb, 
-        year, month, day, hour, minute, second, mostempr;
-    char base_st[TEXT_SENSOR_MAX_LEN], volt_st[TEXT_SENSOR_MAX_LEN], curr_st[TEXT_SENSOR_MAX_LEN],
-        temp_st[TEXT_SENSOR_MAX_LEN], date[TEXT_SENSOR_MAX_LEN], time[TEXT_SENSOR_MAX_LEN],
-        bv_st[TEXT_SENSOR_MAX_LEN], bt_st[TEXT_SENSOR_MAX_LEN], mos_st[TEXT_SENSOR_MAX_LEN];
+    int bat_num = 0, 
+        volt, 
+        curr, 
+        tempr, 
+        tlow, 
+        thigh, 
+        vlow, 
+        vhigh, 
+        coulomb, 
+        year, month, day, hour, minute, second, 
+        mostempr,
+
+        //systeme_is, //char
+        total_num,
+        value_present_num,
+        sleep_num,
+        system_volt,
+        system_curr,
+        system_rc,
+        system_fcc,
+        system_soc,
+        system_soh,
+        highest_voltage,
+        average_voltage,
+        lowest_voltage,
+        highest_temperature,
+        average_temperature,
+        lowest_temperature,
+        recommend_chg_voltage,
+        recommend_dsg_voltage,
+        recommend_chg_current,
+        recommend_dsg_current,
+        system_recommend_chg_voltage,
+        system_recommend_dsg_voltage,
+        system_recommend_chg_current,
+        system_recommend_dsg_current,
+        ;
+    char base_st[TEXT_SENSOR_MAX_LEN], 
+         volt_st[TEXT_SENSOR_MAX_LEN], 
+         curr_st[TEXT_SENSOR_MAX_LEN],
+         temp_st[TEXT_SENSOR_MAX_LEN], 
+         date[TEXT_SENSOR_MAX_LEN], 
+         time[TEXT_SENSOR_MAX_LEN],
+         bv_st[TEXT_SENSOR_MAX_LEN], 
+         bt_st[TEXT_SENSOR_MAX_LEN], 
+         mos_st[TEXT_SENSOR_MAX_LEN],
+        systeme_is[TEXT_SENSOR_MAX_LEN], //char
+        ;
   };
 
   virtual void on_line_read(LineContents *line);

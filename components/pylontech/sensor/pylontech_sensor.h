d465b179c772eed52ad61a2cd6e4e869842a9dfd
だから <82636574+Dackara@users.noreply.github.com>
2024-01-09 12:59:44 +0100

    Update pylontech_sensor.h

--- components/pylontech/sensor/pylontech_sensor.h
+++ components/pylontech/sensor/pylontech_sensor.h
@@ -11,6 +11,7 @@ class PylontechSensor : public PylontechListener, public Component {
   PylontechSensor(int8_t bat_num);
   void dump_config() override;
 
+// pwr sensor
   SUB_SENSOR(voltage)
   SUB_SENSOR(current)
   SUB_SENSOR(temperature)
@@ -20,6 +21,31 @@ class PylontechSensor : public PylontechListener, public Component {
   SUB_SENSOR(voltage_high)
   SUB_SENSOR(coulomb)
   SUB_SENSOR(mos_temperature)
+
+// pwrsys sensor
+  SUB_SENSOR(value_total_num)
+  SUB_SENSOR(value_present_num)
+  SUB_SENSOR(value_sleep_num)
+  SUB_SENSOR(value_system_volt)
+  SUB_SENSOR(value_system_curr)
+  SUB_SENSOR(value_system_rc)
+  SUB_SENSOR(value_system_fcc)
+  SUB_SENSOR(value_system_soc)
+  SUB_SENSOR(value_system_soh)
+  SUB_SENSOR(value_highest_voltage)
+  SUB_SENSOR(value_average_voltage)
+  SUB_SENSOR(value_lowest_voltage)
+  SUB_SENSOR(value_highest_temperature)
+  SUB_SENSOR(value_average_temperature)
+  SUB_SENSOR(value_lowest_temperature)
+  SUB_SENSOR(value_recommend_chg_voltage)
+  SUB_SENSOR(value_recommend_dsg_voltage)
+  SUB_SENSOR(value_recommend_chg_current)
+  SUB_SENSOR(value_recommend_dsg_current)
+  SUB_SENSOR(valu_systee_recommend_chg_voltage)
+  SUB_SENSOR(value_syste_recommend_dsg_voltage)
+  SUB_SENSOR(value_syste_recommend_chg_current)
+  SUB_SENSOR(value_syste_recommend_dsg_current)
 
   void on_line_read(LineContents *line) override;
 

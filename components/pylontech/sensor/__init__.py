c3def0e4eeb7885927162be4f2c1de10471027e0
だから <82636574+Dackara@users.noreply.github.com>
2024-01-09 06:53:24 +0100

    Update __init__.py

--- components/pylontech/sensor/__init__.py
+++ components/pylontech/sensor/__init__.py
@@ -25,6 +25,8 @@ from .. import (
 
 PylontechSensor = pylontech_ns.class_("PylontechSensor", cg.Component)
 
+CONF_UNIT_AMPERE_HOUR = "Ah"
+# pwr sensors
 CONF_TEMPERATURE_LOW = "temperature_low"
 CONF_TEMPERATURE_HIGH = "temperature_high"
 CONF_CELL_LOW = "cell_low"
@@ -33,6 +35,31 @@ CONF_COULOMB = "coulomb"
 CONF_VOLTAGE_LOW = "voltage_low"
 CONF_VOLTAGE_HIGH = "voltage_high"
 CONF_MOS_TEMPERATURE = "mos_temperature"
+
+# pwrsys sensors
+CONF_TOTAL_NUM = "total_num"
+CONF_VALUE_PRESENT_NUM = "value_present_num"
+CONF_SLEEP_NUM = "sleep_num"
+CONF_SYSTEM_VOLT = "system_volt"
+CONF_SYSTEM_CURR = "system_curr"
+CONF_SYSTEM_RC = "system_rc"
+CONF_SYSTEM_FCC = "system_fcc"
+CONF_SYSTEM_SOC = "system_soc"
+CONF_SYSTEM_SOH = "system_soh"
+CONF_HIGHEST_VOLTAGE = "highest_voltage"
+CONF_AVERAGE_VOLTAGE = "average_voltage"
+CONF_LOWEST_VOLTAGE = "lowest_voltage"
+CONF_HIGEST_TEMPERATURE = "highest_temperature"
+CONF_AVERAGE_TEMPERATURE = "average_temperature"
+CONF_LOWEST_TEMPERATUREE = "lowest_temperature"
+CONF_RECOMMEND_CHG_VOLTAGE = "recommend_chg_voltage"
+CONF_RECOMMEND_DSG_VOLTAGE = "recommend_dsg_voltage"
+CONF_RECOMMEND_CHG_CURRENT = "recommend_chg_current"
+CONF_RECOMMEND_DSG_CURRENT = "recommend_dsg_current"
+CONF_SYSTEM_RECOMMEND_CHG_VOLTAGE = "system_recommend_chg_voltage"
+CONF_SYSTEM_RECOMMEND_DSG_VOLTAGE = "system_recommend_dsg_voltage"
+CONF_SYSTEM_RECOMMEND_CHG_CURRENT = "system_recommend_chg_current"
+CONF_SYSTEM_RECOMMEND_DSG_CURRENT = "system_recommend_dsg_current"
 
 TYPES: dict[str, cv.Schema] = {
     CONF_VOLTAGE: sensor.sensor_schema(
@@ -79,6 +106,117 @@ TYPES: dict[str, cv.Schema] = {
         unit_of_measurement=UNIT_CELSIUS,
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_TEMPERATURE,
+    ),
+
+    # pwrsys sensor
+    CONF_TOTAL_NUM: sensor.sensor_schema(
+        accuracy_decimals=0,
+    ),
+    CONF_VALUE_PRESENT_NUM: sensor.sensor_schema(
+        accuracy_decimals=0,
+    ),
+    CONF_SLEEP_NUM: sensor.sensor_schema(
+        accuracy_decimals=0,
+    ),
+    CONF_SYSTEM_VOLT: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_SYSTEM_CURR: sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_CURRENT,
+    ),
+    CONF_SYSTEM_RC: sensor.sensor_schema(
+        unit_of_measurement=AMPERE_HOUR,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_CURRENT,
+    ),
+    CONF_SYSTEM_FCC: sensor.sensor_schema(
+        unit_of_measurement=AMPERE_HOUR,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_CURRENT,
+    ),
+    CONF_SYSTEM_SOC: sensor.sensor_schema(
+        unit_of_measurement=UNIT_PERCENT,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_BATTERY,
+    ),
+    CONF_SYSTEM_SOH: sensor.sensor_schema(
+        unit_of_measurement=UNIT_PERCENT,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_BATTERY,
+    ),
+    CONF_HIGHEST_VOLTAGE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_AVERAGE_VOLTAGE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_LOWEST_VOLTAGE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_HIGEST_TEMPERATURE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+    ),
+    CONF_AVERAGE_TEMPERATURE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+    ),
+    CONF_LOWEST_TEMPERATUREE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+    ),
+    CONF_RECOMMEND_CHG_VOLTAGE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_RECOMMEND_DSG_VOLTAGE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_RECOMMEND_CHG_CURRENT: sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_CURRENT,
+    ),
+    CONF_RECOMMEND_DSG_CURRENT: sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_CURRENT,
+    ),
+    CONF_SYSTEM_RECOMMEND_CHG_VOLTAGE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_SYSTEM_RECOMMEND_DSG_VOLTAGE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_SYSTEM_RECOMMEND_CHG_CURRENT: sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_CURRENT,
+    ),
+    CONF_SYSTEM_RECOMMEND_DSG_CURRENT: sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_CURRENT,
     ),
 }
 

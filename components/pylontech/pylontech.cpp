eaf967551871bf0b91078c32e8051429a50cecb9
だから <82636574+Dackara@users.noreply.github.com>
2024-01-10 06:31:28 +0100

    Update pylontech.cpp

--- components/pylontech/pylontech.cpp
+++ components/pylontech/pylontech.cpp
@@ -31,7 +31,7 @@ void PylontechComponent::setup() {
   }
 }
 
-void PylontechComponent::update() {}//{ this->write_str("pwr\n"); }
+void PylontechComponent::update() { this->write_str("pwr\n"); }
 
 void PylontechComponent::loop() {
   uint8_t data;

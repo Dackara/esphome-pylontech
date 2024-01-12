4d8c2f7a7720831fe4483627ac4c67c4d3e2a67c
だから <82636574+Dackara@users.noreply.github.com>
2024-01-12 07:22:58 +0100

    Update pylontech.cpp

--- components/pylontech/pylontech.cpp
+++ components/pylontech/pylontech.cpp
@@ -75,7 +75,7 @@ void PylontechComponent::process_line_(std::string &buffer) {
   PylontechListener::LineContents l{};
   char mostempr_s[6];
   const int parsed = sscanf(                                                                                   // NOLINT
-      buffer.c_str(), "%d %d %d %d %d %d %d %d %7s %7s %7s %7s %d%% %*d-%*d-%*d %*d:%*d:%*d %7s %7s %d %7s",   // NOLINT
+      buffer.c_str(), "%d %d %d %d %d %d %d %d %7s %7s %7s %7s %d%% %*d-%*d-%*d %*d:%*d:%*d %7s %7s %5s %7s",   // NOLINT
       &l.bat_num, &l.volt, &l.curr, &l.tempr, &l.tlow, &l.thigh, &l.vlow, &l.vhigh, l.base_st, l.volt_st,      // NOLINT
       l.curr_st, l.temp_st, &l.coulomb, l.bv_st, l.bt_st, mostempr_s, l.mos_st);                              // NOLINT
       //l.curr_st, l.temp_st, &l.ccoulomb, &l.year, &l.month, &l.day, &l.hour, &l.minute, &l.second, l.bv_st,  // NOLINT

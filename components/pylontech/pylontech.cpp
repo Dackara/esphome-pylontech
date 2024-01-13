50f12fd7adbc5be578dc1313f633a1b9651a050e
だから <82636574+Dackara@users.noreply.github.com>
2024-01-13 19:58:06 +0100

    Update pylontech.cpp

--- components/pylontech/pylontech.cpp
+++ components/pylontech/pylontech.cpp
@@ -97,9 +97,8 @@ void PylontechComponent::process_line_(std::string &buffer) {
       ESP_LOGW(TAG, "bat_num %d: received no mostempr", l.bat_num);
     }
   
-  auto mos_st_parsed = parse_number<char*>(mos_st_s);
     if (mos_st_s == "Normal" or "Alarm!" or "Idle" or "Absent") {
-      l.mos_st = mos_st_parsed.value();
+      l.mos_st = <char*>(mos_st_s);
     } else {
       l.mos_st == "NULL";
       ESP_LOGW(TAG, "bat_num %d: received no mos_st", l.bat_num);

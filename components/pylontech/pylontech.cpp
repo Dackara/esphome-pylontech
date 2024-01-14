f92ffcfa4657ceb0b070b11459db27f5be0aaf97
だから <82636574+Dackara@users.noreply.github.com>
2024-01-14 08:45:18 +0100

    Update pylontech.cpp

--- components/pylontech/pylontech.cpp
+++ components/pylontech/pylontech.cpp
@@ -82,7 +82,7 @@ void PylontechComponent::process_line_(std::string &buffer) {
       //l.curr_st, l.temp_st, &l.ccoulomb, &l.year, &l.month, &l.day, &l.hour, &l.minute, &l.second, l.bv_st, // NOLINT
   
   if (l.bat_num <= 0) {
-    if (sscanf(l.bat_num(), "Power")) {
+    if (sscanf(l.bat_num, "Power")) {
       ESP_LOGD(TAG, "String pwr OK");
     }
     else {

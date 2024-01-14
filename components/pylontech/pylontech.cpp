f4c5bb0ce530357f0c82fffdfc9ccff0bf12bbff
だから <82636574+Dackara@users.noreply.github.com>
2024-01-14 08:34:28 +0100

    Update pylontech.cpp

--- components/pylontech/pylontech.cpp
+++ components/pylontech/pylontech.cpp
@@ -82,8 +82,14 @@ void PylontechComponent::process_line_(std::string &buffer) {
       //l.curr_st, l.temp_st, &l.ccoulomb, &l.year, &l.month, &l.day, &l.hour, &l.minute, &l.second, l.bv_st, // NOLINT
   
   if (l.bat_num <= 0) {
-    ESP_LOGD(TAG, "invalid bat_num in line %s", buffer.substr(0, buffer.size() - 2).c_str());
+    if (l.bat_num == "Power") {
+      ESP_LOGD(TAG, "String pwr OK");
+    }
+    else {
+      ESP_LOGD(TAG, "invalid bat_num in line %s", buffer.substr(0, buffer.size() - 2).c_str());
+    }
     return;
+
   }
   if (parsed != 17) {
     ESP_LOGW(TAG, "invalid line: found only %d items in %s", parsed, buffer.substr(0, buffer.size() - 2).c_str());
